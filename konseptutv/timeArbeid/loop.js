db7e6537b02efe488ea9f33d6834e72d53a5ebbc
Kammi03 <146733563+Kammi03@users.noreply.github.com>
2024-02-06 14:52:57 +0100

    Add files via upload

---
+++ konseptutv/timeArbeid/loop.js
@@ -0,0 +1,29 @@
+//oppgave 1.
+
+//for (let i = 1; i <= 10; i++) { 
+
+   //console.log(i); } 
+
+
+
+//oppgave 2.
+
+//for (let i = 2; i <= 10; i+=2) { 
+
+    //console.log(i); } 
+
+
+
+//oppgave 3.
+
+//for (let i = 2; i <= 20; i+=3) { 
+
+    //console.log(i); } 
+
+
+
+//oppgave 4.
+
+for (let i = "*"; i <= "*****"; i=i+"*") { 
+
+    console.log(i); } 
---
+++ konseptutv/timeArbeid/script.js
@@ -0,0 +1,16 @@
+// let number = 0;
+// if (number ==0) {
+//     console.log("tallet er null");
+// } else if (number <=0) {
+//     console.log("tallet er negativt");
+// } else{
+//     console.log("tallet er positivt");
+// }
+let age = 65;
+if (age < 18) {
+    console.log("under 18");
+} else if (age >= 18 && age <= 65) {
+    console.log("arbeids alder");
+} else {
+    console.log("berre pensjonist");
+}
