d6620e758061216fe68ef44b25bdb16a7934a80b
Kammi03 <146733563+Kammi03@users.noreply.github.com>
2024-02-13 15:13:10 +0100

    Create meny.js
    dfg

---
+++ konseptutv/yff/meny.js
@@ -0,0 +1,18 @@
+
+$(function() {
+    $('li > ul').each(function(i) {
+
+        var parentLi = $(this).parent('li');
+
+        parentLi.addClass('folder');
+
+        var subUl = $(this).remove();
+        parentLi.wrapInner('<a/>').find('a').click(function() {
+            subUl.toggle();
+        });
+        parentLi.append(subUl);
+    });
+
+  
+    $('ul ul').hide();
+});
