ef5d04d82536b0fd9066be4878ee9994d2d45620
Kammi03 <146733563+Kammi03@users.noreply.github.com>
2025-01-15 12:39:49 +0100

    Delete konseptutv directory

--- konseptutv/timeArbeid/502.js
+++
@@ -1,8 +0,0 @@
-let m = (Math.random()*10).toFixed(2)
-let dm = (m*10).toFixed(1)
-let cm = (m*100).toFixed(0)
-let mm = (m*1000).toFixed(0)
-
-console.log("en lende på" + m + "m=" + dm + "dm");
-console.log("en lende på" + m + "m=" + cm + "cm");
-console.log("en lende på" + m + "m=" + mm + "mm");
